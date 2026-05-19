5fd19a3082eae8479bbe0929b572aa44b4386e37
Clement CHOLLET <clement.chollet@epita.fr>
2026-05-19 17:07:21 +0200

    feat(flip): added a 'flip' command

---
+++ modules/fun/flip.py
@@ -0,0 +1,42 @@
+"""
+The `flip` command.
+On it, it flips a coins and either prints "Heads!" or "Tails"
+"""
+
+from random import choice, randint
+
+import asyncio
+from discord.ext import commands
+
+
+class Flip(commands.Cog):
+    """
+    The `flip` command class.
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def flip(self, ctx):
+        """
+        The `flip` command.
+        """
+
+        msg = await ctx.send("Flipping the coin...")
+
+        results = ["Heads", "Tails"]
+        for i in range(randint(4, 8)):
+            await asyncio.sleep(.5)
+            await msg.edit(content=f"{results[i % 2]}")
+
+        await msg.edit(content=f"It landed on **{choice(results)}**!")
+
+
+async def setup(bot):
+    """
+    The function used to load the `flip` command.
+    """
+
+    print("Loading `flip` command from `fun` module.")
+    await bot.add_cog(Flip(bot))
